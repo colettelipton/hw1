06d546a946c52a7ba2b1dd39897e737bad711bf0
Colette Lipton <clipton@usc.edu>
2023-01-19 00:18:49 +0000

    all done with part 1

--- split.cpp
+++ split.cpp
@@ -11,13 +11,62 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef>
+using namespace std;
 
 /* Add a prototype for a helper function here if you need */
 
+void push(Node*& head, Node*& append){
+    if(head->next == nullptr){
+        head->next = append;
+        return;
+    }
+    push(head->next, append);
+}
+
 void split(Node*& in, Node*& odds, Node*& evens)
 {
-  /* Add code here */
-// WRITE YOUR CODE HERE
+    bool ch = false;
+    if(in == nullptr){
+        in = NULL;
+        return;
+    }
+    
+    if(evens == NULL || odds == NULL){
+        if(evens == NULL){
+            if(!ch && in->value % 2 == 0){
+                evens = in;
+                in = in->next;
+                evens->next = nullptr;
+                ch = true;
+            }
+        }
+        if(odds == NULL){
+            if(!ch && in->value % 2 == 1){
+                odds = in;
+                in = in->next;
+                odds->next = nullptr;
+                ch = true;
+            }
+        }
+    }
+    
+    if(!ch && !(evens == nullptr) && in->value % 2 == 0){
+        push(evens,in);
+        Node* temp = in;
+        in = in->next;
+        temp->next = nullptr;
+        ch = true;
+    }
+    
+    if(!ch && !(odds == nullptr) && in->value % 2 == 1){
+        push(odds,in);
+        Node* temp = in;
+        in = in->next;
+        temp->next = nullptr;
+        ch = true;
+    }
+    split(in,odds,evens);
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -9,9 +9,41 @@ You can compile this file like this:
 g++ split.cpp test_split.cpp -o test_split
 */
 
+#include <iostream>
 #include "split.h"
+using namespace std;
 
 int main(int argc, char* argv[])
 {
+ Node* n5 = new Node(8, nullptr);
+    Node* n4 = new Node(4, n5);
+    Node* n3 = new Node(6, n4);
+    Node* n2 = new Node(2, n3);
+    Node* n1 = new Node(1, n2);
+    
+    Node* odds = NULL;
+    Node* evens = NULL;
+    
+    split(n1, odds, evens);
+    
+    Node* cur = odds;
+    cout << "odds" << endl;
+    while(cur != nullptr){
+        Node* temp = cur;
+        cout << cur->value << endl;
+        cur = cur->next;
+        delete temp;
+    }
+    
+    cur = evens;
+    cout << "evens" << endl;
+    while(cur != nullptr){
+        Node* temp = cur;
+        cout << cur->value << endl;
+        cur = cur->next;
+        delete temp;
+    }
+    
+    return 0;
 
 }
